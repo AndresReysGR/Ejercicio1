e43507de980884a249d00485371148aebaddeb2d
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2019-08-15 07:48:12 -0700

    Agregar archivos de proyecto.

---
+++ Ejercicio1/Enemigo.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio1
+{
+    class Enemigo
+    {
+        public int x { get; set; }
+        public int y { get; set; }
+        
+        public int _ganas;
+
+        public int Ganas
+        {
+            get
+            {
+                return _ganas;
+            }
+            set
+            {
+                if (value >= 100)
+                {
+                    _ganas = value;
+                }
+                else
+                {
+                    throw new Exception("No es mayor de 100");
+                }
+            }
+        }
+        public moverse()
+        {
+
+        }
+        public disparo()
+        {
+
+        }
+        public explotar()
+        {
+
+        }
+    }
+}
---
+++ Ejercicio1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Enemigo enemigo
+                = new Enemigo();
+            enemigo.x = 500;
+            enemigo.y = 800;
+
+            Console.WriteLine("X: " + enemigo.x);
+            Console.WriteLine("y: " + enemigo.y);
+
+            try
+            {
+                enemigo.Ganas = 5000;
+                Console.WriteLine("Ganas: " + enemigo.Ganas);
+            }
+            catch
+            {
+                Console.WriteLine("Debes tener mas de 100 puntos");
+            }
+            Console.Read();
+        }
+    }
+}
